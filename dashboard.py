defc37567980a0b6b8f9c570b938370692318049
Fabio Oppini <fabio.oppini@gmail.com>
2021-11-03 21:51:54 +0100

    Small fixes

--- dashboard.py
+++ dashboard.py
@@ -21,8 +21,7 @@ def load_data():
     df['text_name'] = df.apply(lambda row: get_text_name(row), axis=1)
     df.dropna(inplace=True)
     ##df_regional.drop(columns=['data_string'], inplace=True)
-    df.rename(columns={'refreshed_at': 'refreshed_at_string'}, inplace=True)
-    df['refreshed_at'] = df.apply (lambda row: datetime.datetime.strptime(row['refreshed_at_string'],"%Y-%m-%d %H:%M:%S.%f").date(), axis=1)
+    df['refreshed_on'] = df.apply (lambda row: datetime.datetime.strptime(row['refreshed_at'],"%Y-%m-%d %H:%M:%S.%f").date(), axis=1)
 
     for column in ('odds_1', 'odds_X', 'odds_2', '1X2 Vig',
         'odds_1X', 'odds_X2', 'odds_12', 'DC Vig',
@@ -49,15 +48,15 @@ def main():
     one_week_back = datetime.date.today() + datetime.timedelta(days=-2)
     start_date = st.sidebar.date_input('Eventi refreshati a partire da:', one_week_back)
 
-    show_labels = st.sidebar.checkbox('Show labels')
-    label_length = st.sidebar.slider('Label characters:', 1, 10, 3)
+    show_labels = st.sidebar.checkbox('Mostra nomi squadre nel grafico')
+    label_length = st.sidebar.slider('Abbreviazione nomi squadre (caratteri):', 1, 10, 3)
     df['text_name'] = df.apply(lambda row: get_text_name(row, label_length), axis=1)
 
     st.header(f"{league_select} {betting_select}")
 
     #st.table(df[df['league'] == league_select])
-    df_filtered = df[(df['league'] == league_select) & (df['refreshed_at'] >= start_date)]
-    st.table(df_filtered[['home_team', 'away_team', 'refreshed_at'] + betting_columns[betting_select]])
+    df_filtered = df[(df['league'] == league_select) & (df['refreshed_on'] >= start_date)]
+    st.table(df_filtered[['home_team', 'away_team', 'refreshed_on'] + betting_columns[betting_select]])
 
     chart = None
     if betting_select == 'Gol/Nogol':
@@ -67,9 +66,9 @@ def main():
             chart = px.scatter(df_filtered, x="odds_gol", y="odds_nogol", color="G/NG Vig", hover_name="hover_name")
     elif betting_select == 'Under/Over 2.5':
         if show_labels:
-            chart = px.scatter(df[df['league'] == league_select], x="odds_under_2.5", y="odds_over_2.5", color="U/O 2.5 Vig", hover_name="hover_name")
-        else:
             chart = px.scatter(df[df['league'] == league_select], x="odds_under_2.5", y="odds_over_2.5", color="U/O 2.5 Vig", text="text_name", hover_name="hover_name")
+        else:
+            chart = px.scatter(df[df['league'] == league_select], x="odds_under_2.5", y="odds_over_2.5", color="U/O 2.5 Vig", hover_name="hover_name")
     elif betting_select == '1X2':
         if show_labels:
             chart = px.scatter_3d(df[df['league'] == league_select], x="odds_1", y="odds_X", z="odds_2",
